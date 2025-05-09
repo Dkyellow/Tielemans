034e7b1b11821ebf0a7da15266891a5a1a32e1ed
Dkyellow <lesliesarai321@gmail.com>
2025-05-09 14:28:56 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let menubtn = document.querySelector(".menu-btn");
-let exitbtn = document.querySelector(".exit");
-let mobilenavbar = document.querySelector(".mobile-nav-bar");
-menubtn.addEventListener("click", () => {
-  mobilenavbar.style.display = "block";
-});
-exitbtn.addEventListener("click", () => {
-  mobilenavbar.style.display = "none";
-});
